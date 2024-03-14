eb61dcab5bea02e4cda87e14274f0f75e33a8893
是阿玮呀 <1316391640@qq.com>
2024-03-14 09:00:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div> 123 </div>
+  <div
+    class="aa"
+    style=""
+    @click="
+      () => {
+        const a = 1
+      }
+    "
+    >2342342342342342342342342werwer34234234</div
+  >
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import Antd from 'ant-design-vue'
+import components from './components/index'
+import 'reset-css'
+import '@/assets/style/varable.scss'
+import 'ant-design-vue/dist/reset.css'
+import App from '@/App.vue'
+
+createApp(App).use(Antd).use(components).mount('#app')
